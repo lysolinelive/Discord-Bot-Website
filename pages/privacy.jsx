7facf4f9dc8f428b67e21ce30dae4f31b4b90eb7
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 11:43:32 +0300

    Update privacy.jsx

--- pages/privacy.jsx
+++ pages/privacy.jsx
@@ -5,7 +5,7 @@ export default function Privacy() {
             <h1 className="font-semibold text-3xl text-white/75">Privacy Policy for Astra</h1>
             <p className="text-sm text-white/30 mb-4">Last Update: 20/05/2022  🆕️</p>
   <div className="animateHeader text-white text-opacity-40 mt-5">
-        <p>⊳ To use the Astra bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
+        <p>⊳ To use the Lysoline bot, you must define the necessary permissions when adding it to your discord server. <a className="text-white">(READ MESSAGES, SEND MESSAGES, EMBED LINKS, ATTACH FILES, READ MESSAGE HISTORY, CONNECT, SPEAK, USE VOICE ACTIVITY, PRIORITY SPEAKER)</a></p>
         <div className="py-3"></div>
         <p>⊳ You have to vote for the <a className="text-white" href="">Lysoline</a> bot when using certain commands from the bot commands.</p>
         <div className="py-3"></div>
