40810c130004b908d962d35536063c47b9bc8a3b
lysolinelive <141401876+lysolinelive@users.noreply.github.com>
2023-08-04 14:51:05 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,74 +1,332 @@
-import Link from "next/link";
 import Image from "next/image";
 
-export default function Index() {
-	
-  return (
-    <>
-      <div className="py-20 mb-30 max-w-3xl mx-auto">
-        <div className="flex w-full justify-center">
-      <img width="500" src="/img/bck.png" />
-      </div>
-        <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Lysoline Bot
-        </p>
-        <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-       How about moderating your own server with Lysoline bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
-	  </p>
-        
+// ...
 
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-y-12 lg:gap-y-0 lg:gap-x-12 py-24">
-          <div className="col-span-4">
-            <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Lysoline Bot" }} />
-            <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.gg/UUwP84WD" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Lysoline
-            </a>
-          </div>
+<Image src="/img/bck.png" width={500} height={300} alt="Background Image" />
+import Link from "next/link";
 
-          <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
-           
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
-                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, Lysoline gives you a musical pleasure to the fullest.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
-                  <p className="text-gray-500 line-clamp-4">A bot that supports multi-platform music playback. (Spotify, Souncloud, YouTube) | (It is a bot that complies with all copyrights and licenses.)</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
-                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop.</p>
-                </div>
-              </div>
-              <div>
-                <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Support</p>
-                  <p className="text-gray-500 line-clamp-4">With our best teammates, we provide the fastest response to anyone who needs help.</p>
-                </div>
-              </div>
-          </div>
-      </div>
+// ...
 
 
 
-      <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
-              <>
-                <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/1014356370776137812/1136937607780106290/L.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Lysoline, the best and most up-to-date bot?</p>
-                </div>
-                <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
-              </>
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab fa-discord mr-2" />Invite Lysoline
+ 
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab fa-discord mr-2" />Invite Lysoline
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab fa-discord
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab fa-disc
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+   
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"
+
+
+<Link href="https://discord.gg/UUwP84WD">
+  <a className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+    <i className="fab fa-discord mr-2" />Invite Lysoline
+  </a>
+</Link>
+	  <div className="col-span-8 grid grid-cols-1 lg:grid-cols-2 gap-6">
+  {[
+    {
       
-        </div>
+  {[
+    {
+     
 
-      </>
-  )
-}
+  {[
+    {
+
+ 
+icon: "fa-cogs",
+      
+      title
+title: "Optimization",
+      
+     
+description: "With fast and customizable settings specific to your server, Lysoline gives you a musical pleasure to the fullest.",
+    },
+    
+    },
+   
+
+    },
+// Diğer öğeleri buraya ekleyin
+  ].
+ 
+map((item, index) => (
+    
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className="text-gray-500 line-clamp-4">{item.description}</p
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className="text-gray-500 line
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className="text-gray-
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className="
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+       
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+       
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+         
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+
+    <div key={index}>
+      <div className="text-white">
+        <p className="text-xl font
+
+    <div key={index}>
+      <div className="text-white">
+        <p className
+
+    <div key={index}>
+      <div className="text-white">
+        <p
+
+    <div key={index}>
+      <div className="text-white">
+       
+
+    <div key={index}>
+      <div className="text-white">
+
+   
+<div key={index}>
+      <div className="text-white">
+        <p className="text-xl font-semibold mt-5">
+          <i className={`fa ${item.icon} text-2xl text-amber-500`} /> {item.title}
+        </p>
+        <p className="text-gray-500 line-clamp-4">{item.description}</p>
+      </div>
+    </div>
+  ))}
+</div>
+
+  ))}
+</
+
+  ))}
+
+ 
